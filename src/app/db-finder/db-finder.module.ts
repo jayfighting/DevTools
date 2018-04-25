9299b273ced12eafb00e3787997858f8ccb0dea2
Collin Stevens <costevens@loandepot.com>
2018-04-24 18:54:27 -0700

    Initial commit

---
+++ src/app/db-finder/db-finder.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DbFinderRoutingModule } from './db-finder-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DbFinderRoutingModule
+  ],
+  declarations: []
+})
+export class DbFinderModule { }
---
+++ src/app/loan-copy/loan-copy.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoanCopyRoutingModule } from './loan-copy-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LoanCopyRoutingModule
+  ],
+  declarations: []
+})
+export class LoanCopyModule { }
