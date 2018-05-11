1f603a9082a4f714ee8ffe768c125d1d82455032
Hang Zhao <hzhao@loandepot.com>
2018-05-11 14:52:35 -0700

    Add Mock up for loan-copy ui

--- src/app/loan-copy/loan-copy.module.ts
+++ src/app/loan-copy/loan-copy.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { LoanCopyRoutingModule } from './loan-copy-routing.module';
 import { LoanCopyPageComponent } from './containers/loan-copy-page/loan-copy-page.component';
-import { MaterialModule } from '../shared/material.module'
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
     LoanCopyRoutingModule,
-    MaterialModule
+    SharedModule
   ],
   declarations: [
     LoanCopyPageComponent
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { Directives } from './directives';
 import { Pipes } from './pipes';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    FlexLayoutModule,
+    MaterialModule
+  ],
+  exports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule
+  ],
   declarations: []
 })
-export class SharedModule { }
+export class SharedModule {}
